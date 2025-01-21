1702bcbf089356c91162f63631ba8b7fd6e957b3
Raul Primo <raul.primoimp@gmail.com>
2025-01-21 09:55:36 -0300

    gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/WebBooks/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 });
